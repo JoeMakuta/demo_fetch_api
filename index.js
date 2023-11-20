288a4dae874d61956f4d6b7f16f2edfe7dee6ac4
JoeMakuta <makutajosue@gmail.com>
2023-11-20 22:53:07 +0200

    Feat : dog app

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-const a = [1, 2, 3, 4, 5];
+const DOG_URL = "https://dog.ceo/api/breeds/image/random";
 
-c = a + "h";
-console.log(c, "Conflit");
+const doggos = document.querySelector(".dog-gallery");
+const button = document.querySelector(".add-dog");
+
+function addNewDoggo() {
+  fetch(DOG_URL)
+    .then(function (response) {
+      const processingPromise = response.json();
+      return processingPromise;
+    })
+    .then(function (processedResponse) {
+      const img = document.createElement("img");
+      img.src = processedResponse.message;
+      img.alt = "Cute doggo";
+      img.className = "image";
+      doggos.appendChild(img);
+    });
+}
+
+button.addEventListener("click", addNewDoggo);
